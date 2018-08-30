2f93b5d60b3cc4e784d7162fafa956d2fe702e8e
Sabatr <bnguyen12152@gmail.com>
2018-08-31 03:41:36 +1200

    Changed to pets

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,21 @@
+
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+import PlayCircleFilled from '@material-ui/icons/PlayCircleFilled';
+
 import * as React from 'react';
 import './App.css';
 
-interface IState {
-  // this is used to get the image of the pokemon//
-  imageFiles: any,
-  // the name the user entered//
-  input: any,
-  // result in what pokemon you are//
-  results: any
 
+interface IState {
+  animal: any,
+  ok: any,
+  results: any
 }
 
 export default class App extends React.Component<{},IState> {
@@ -16,65 +23,145 @@ export default class App extends React.Component<{},IState> {
     super(props)
 
     this.state = {
-      imageFiles: [],
-      input: "",
-      results: ""
+      animal: "",
+      ok: "",
+      results: "",
     }
 
-    this.getApi = this.getApi.bind(this);
+   //  this.getDogApi = this.getDogApi.bind(this);
   }
 
-  public handleChange = (event: any) => {
+  public async getDogApi() {
+    const dogApi = await fetch('https://random.dog/woof.json');
+    const data= await dogApi.json();
     this.setState({
-      input: event.target.value,
-    });
+      ok: "ok",
+      results: data.url
+
+    })
+    if (this.state.results.includes(".mp4")) {
+      this.getDogApi();
+    }
   }
 
-  public findPokemon = async () => {
-    // const Pokedex = require('pokeapi-js-wrapper');
-    // const P = new Pokedex.Pokedex();
-   // const pokemon = P.getPokemonByName('eevee');
-    // alert(P.getPokemonByName('eevee').forms);
-    // this.getApi;
+  public async getCatApi() {
+    const catApi = await fetch('https://aws.random.cat/meow');
+    const data= await catApi.json();
+    this.setState({
+      ok: "ok",
+      results: data.file
+
+    })
+  }
+
+  public async getFoxApi() {
+    const foxApi = await fetch('https://randomfox.ca/floof/');
+    const data= await foxApi.json();
+    this.setState({
+      ok: "ok",
+      results: data.image
+
+    })
+  }
+  
+
+  public handleChange = (event:any) => {
+    this.setState({
+      animal: event.target.value
+    })
+  }
+
+  public handleClick = () => {
+    this.setState({
+      ok: "not ok"
+    })
+    switch(this.state.animal) {
+      case "Dogs":
+        this.getDogApi();
+        break;
+      case "Cats":
+        this.getCatApi();
+        break;
+      case "Foxes":
+        this.getFoxApi();
+        break;
+      default:
+        break;
+    } 
+  }
+
+  public createImage() {
+    
+    const body = document.getElementsByTagName('body')[0]
+    const image = document.createElement('img');
+    image.src = this.state.results
+    image.alt = this.state.results
+    image.id = "image"
+    body.appendChild(image);
+
+  }
+
+  public removeImage() {
+    const body = document.getElementsByTagName('body')[0]
+    const image = document.getElementById("image")
+    if (image != null) {
+      body.removeChild(image)
+    }
+  }
 
 
-   }
 
-   public getApi() {
-     this.setState({
-       results: "test"
-     });
-  //  fetch('https://pokeapi.co/api/v2/pokemon/eevee')
-     fetch('https://randomuser.me/api/?results=500')
-      .then(output => {
-        return output.json();
-      }).then(data => {
-        // this.setState({
-        //   // results: data.output.map((stat: any) => {
-        //   //   return stat.abiltiies.ability.name; 
-        //   // })
-        // })
-        const pictures = data.output.map((pic: any) => {
-          return (
-            <div key={pic.output}>
-              <img src={pic.picure.medium} />
-             </div>
-          )
-        })
-        this.setState({
-          results:pictures
-        });
-      })
-   }
 
   public render() {
     return (
       <div className="container-fluid">
-        <div className="inputBar">
-          <input id="input" type="text" placeholder="Enter your name." onChange={this.handleChange}/>
-          <button onClick={this.getApi}>Click</button>
-          <p>{this.state.results}</p>
+        <div className="display">
+          {
+              this.state.ok === "not ok" ? (
+              <CircularProgress thickness={3}/>
+            ) : (
+              <img className="image" src={this.state.results} alt={this.state.results}/>
+            )
+          }
+
         </div>
+        <div className="formStuff">
+          <form autoComplete="off" className="form">
+            <div className="DropDown">
+          <FormControl>
+          <InputLabel htmlFor="demo-controlled-open-select">Animal</InputLabel>
+           <Select 
+           style={{
+             fontSize:'30px',
+             height: '50px',
+             width:'350px'
+           }}
+            value={this.state.animal} 
+            onChange={this.handleChange}
+            inputProps={{
+              id: 'demo-controlled-open-select',
+              name: 'animal'
+              
+            }}>
+            <MenuItem value="Dogs">Dogs</MenuItem>
+            <MenuItem value="Cats">Cats</MenuItem>
+            <MenuItem value="Foxes">Foxes</MenuItem>
+          </Select>
+        </FormControl>
+        </div>
+        <div className="Button">
+          <Button style={{
+            fontSize: '30px',
+            height:'50px',
+            width:'50px'
+          }}
+          variant="fab"
+          color= "primary"
+           onClick={this.handleClick}>{<PlayCircleFilled/>}</Button>
+            </div>
+        </form>
+      </div>
+
     </div>
     
     );
